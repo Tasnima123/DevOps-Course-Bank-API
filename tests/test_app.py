aa7090efd17067058759ed233745509a38e878a8
Tasnima123 <Tasnima123@hotmail.co.uk>
2020-12-10 11:52:13 +0000

    add integration tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,10 +9,12 @@ def client():
     app.config['TESTING'] = True
     return app.test_client()
 
-
 def test_account_creation(client):
-    # Use the client to make requests e.g.:
-    # client.post(...)
-    # client.get(...)
-    # https://flask.palletsprojects.com/en/1.1.x/testing/
-    pass
+    # Use the client to make request client.post(...)
+    response_post =client.post('/accounts/test')
+    assert response_post.status_code == 200
+
+def test_account_retrieve(client):   
+    # Use the client to make request client.get(...)
+    response_get = client.get('/accounts/test')
+    assert response_get.status_code == 200
